23dd544416e520a9181c3297c2b56f22a3c97fb7
Billy Lam <0chun0@gmail.com>
2021-01-04 10:36:22 +0800

    Add Pretty print

--- util.go
+++ util.go
@@ -1,7 +1,17 @@
 package myutil
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
+// Hello to return Hello
 func Hello() {
 	fmt.Println("Hello")
+}
+
+// PrettyPrint to print struct in a readable way
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
 }
