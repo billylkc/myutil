c51034f411d783dd32732d1a15c06ad44c1c9c38
Billy Lam <0chun0@gmail.com>
2021-01-04 11:31:54 +0800

    Add simple test

---
+++ util_test.go
@@ -0,0 +1,16 @@
+package myutil
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	tests := []struct {
+		want string
+	}{
+		{"Hello"},
+	}
+	for _, tt := range tests {
+		if got := Hello(); got != tt.want {
+			t.Errorf("Hello() = %q, want %q", got, tt.want)
+		}
+	}
+}
