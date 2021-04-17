4d28194afbe006ccd1aca77a779df90aafc3bb16
Billy Lam <0chun0@gmail.com>
2021-04-17 09:36:44 +0800

    Add shell command util

--- util.go
+++ util.go
@@ -1,9 +1,11 @@
 package myutil
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 // PrettyPrint to print struct in a readable way
@@ -26,4 +28,16 @@ func GetEnv(name string) (string, error) {
 		return "", fmt.Errorf("missing required environment variable " + name)
 	}
 	return v, nil
+}
+
+// Shell calls the shell command
+func Shell(command string) (error, string, string) {
+	const ShellToUse = "bash"
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
 }
